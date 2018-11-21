946e7be0eac36381092200b9a8a8cafa8912c5f9
snowyyu <731444920@qq.com>
2018-11-21 18:40:04 +0800

    前后端产品列表的渲染以及添加到购物车的实现

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var Goods = require('../models/goods');
 
 // 连接MongoDB数据库
-mongoose.connect('mongodb://127.0.0.1:27017/demo', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/dumall', { useNewUrlParser: true });
 mongoose.connection.on("connected", function(){
     console.log("MongoDB connected success")
 });
@@ -13,9 +13,53 @@ mongoose.connection.on("error", function(){
     console.log("MongoDB connected disconnected.")
 });
 
-router.get("/", function(req,res,next){
-    // 借口代码here
-    Goods.find({},function(err,doc){
+router.get("/list", function(req,res,next){
+    //接口代码here
+    // Goods.find({},function(err,doc){
+    //     if(err){
+    //         res.json({
+    //             status:'1',
+    //             msg:err.message
+    //         })
+    //     }else{
+    //         res.json({
+    //             status:'0',
+    //             msg:'',
+    //             result:{
+    //                 count:doc.length,
+    //                 list: doc
+    //             }
+    //         })
+    //     }
+    // })
+    let page = parseInt(req.param("page"));
+    let pageSize = parseInt(req.param("pageSize"));
+    let priceLevel = req.param("priceLevel");
+    let sort = req.param("sort");
+    
+    let skip = (page-1)*pageSize; 
+    let params = {};
+    // 价格区间参数
+    var priceGt = '',priceLte = '';
+    if(priceLevel!='all'){
+        switch (priceLevel){
+            case '0':priceGt = 0;priceLte=100;break;
+            case '1':priceGt = 100;priceLte=500;break;
+            case '2':priceGt = 500;priceLte=1000;break;
+            case '3':priceGt = 1000;priceLte=5000;break;
+        }
+        params = {
+            salePrice:{
+                $gt:priceGt,
+                $lte:priceLte
+            }
+        }
+    }
+    
+    let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+    goodsModel.sort({'salePrice':sort});
+
+    goodsModel.exec(function(err,doc){
         if(err){
             res.json({
                 status:'1',
@@ -34,4 +78,79 @@ router.get("/", function(req,res,next){
     })
     // res.send('hello, goods list here')
 })
+
+
+// 加入到购物车
+router.post("/addCart", function(req,res,next){
+    var userId = '100000077',productId = req.body.productId;
+    var User = require('../models/user');
+    User.findOne({
+        userId:userId
+    },function(err,userDoc){
+        if(err){
+            res.json({
+                status:"1",
+                msg:err.message
+            })
+        }else{
+            console.log("userDoc:"+userDoc);
+            if(userDoc){
+                let goodsItem ='';
+                userDoc.cartList.forEach(function(item){
+                    if(item.productId==productId){
+                        goodsItem = item;
+                        item.productNum ++;
+                    }
+                });
+                if(goodsItem){
+                    userDoc.save(function(err2,doc2){
+                        if(err2){
+                            res.json({
+                                status:"1",
+                                msg:err2.message
+                            })
+                        }else{
+                            res.json({
+                                status:"0",
+                                msg:'',
+                                result:'success'
+                            })
+                        }
+                    })
+                }else{
+                    Goods.findOne({productId:productId},function(err,doc){
+                        if(err){
+                            res.json({
+                                status:"1",
+                                msg:err.message
+                            })
+                        }else{
+                            if(doc){
+                                doc.productNum = 1;
+                                doc.checked =1;
+                                userDoc.cartList.push(doc);
+                                userDoc.save(function(err2,doc2){
+                                    if(err2){
+                                        res.json({
+                                            status:"1",
+                                            msg:err2.message
+                                        })
+                                    }else{
+                                        res.json({
+                                            status:"0",
+                                            msg:'',
+                                            result:'success'
+                                        })
+                                    }
+                                })
+                            }
+                        }
+                    })
+                }
+                
+            }
+        }
+    })
+})
+
 module.exports = router;
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -10,106 +10,207 @@
           <div class="filter-nav">
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
-            <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-            <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            <a href="javascript:void(0)" class="price" @click="sortGoods">Price <svg class="icon icon-arrow-short" v-bind:class="{'sort-up':!sortFlag}"><use xlink:href="#icon-arrow-short"></use></svg></a>
+            <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
           </div>
           <div class="accessory-result">
             <!-- filter -->
-            <div class="filter stopPop" id="filter">
+            <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show': filterBy}">
               <dl class="filter-price">
                 <dt>Price:</dt>
-                <dd><a href="javascript:void(0)">All</a></dd>
-                <dd>
-                  <a href="javascript:void(0)">0 - 100</a>
-                </dd>
-                <dd>
-                  <a href="javascript:void(0)">100 - 500</a>
-                </dd>
-                <dd>
-                  <a href="javascript:void(0)">500 - 1000</a>
-                </dd>
-                <dd>
-                  <a href="javascript:void(0)">1000 - 2000</a>
+                <dd><a href="javascript:void(0)" v-bind:class="{'cur':priceChecked=='all'}" @click="priceChecked='all'">All</a></dd>
+                <dd v-for="(price,index) in priceFilter">
+                  <a href="javascript:void(0)" v-bind:class="{'cur':priceChecked==index}" @click="setPriceFilter(index)">{{price.startPrice}} - {{price.endPrice}}</a>
                 </dd>
               </dl>
             </div>
-
             <!-- search result accessories list -->
             <div class="accessory-list-wrap">
               <div class="accessory-list col-4">
                 <ul>
-                  <li>
+                  <li v-for="item in goodsList">
                     <div class="pic">
-                      <a href="#"><img src="static/1.jpg" alt=""></a>
+                      <a href="#"><img v-bind:src="'/static/'+item.productImage" alt=""></a>
                     </div>
                     <div class="main">
-                      <div class="name">XX</div>
-                      <div class="price">999</div>
+                      <div class="name">{{item.productName}}</div>
+                      <div class="price">{{item.salePrice}}</div>
                       <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="pic">
-                      <a href="#"><img src="static/2.jpg" alt=""></a>
-                    </div>
-                    <div class="main">
-                      <div class="name">XX</div>
-                      <div class="price">1000</div>
-                      <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="pic">
-                      <a href="#"><img src="static/3.jpg" alt=""></a>
-                    </div>
-                    <div class="main">
-                      <div class="name">XX</div>
-                      <div class="price">500</div>
-                      <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="pic">
-                      <a href="#"><img src="static/4.jpg" alt=""></a>
-                    </div>
-                    <div class="main">
-                      <div class="name">XX</div>
-                      <div class="price">2499</div>
-                      <div class="btn-area">
-                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                        <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
                       </div>
                     </div>
                   </li>
                 </ul>
+                <div class="load-more" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="30">
+                  <img src="./../assets/loading-spinning-bubbles.svg" alt="" v-show="loading">
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
+      <modal v-bind:mdShow="mdShow" v-on:close="closeModal">
+        <p slot="message">
+          请先登录，否则无法登录到购物车中！
+        </p>
+        <div slot="btnGroup">
+          <a href="javascript:void(0)" class="btn btn--m" @click="mdShow=false">关闭</a>
+        </div>
+      </modal>
+      <modal v-bind:mdShow="mdShowCart" v-on:close="closeModal">
+        <p slot="message">
+          <svg class="icon-status-ok">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-status-ok"></use>
+          </svg>
+          <span>加入购物车成功！</span>
+        </p>
+        <div slot="btnGroup">
+          <a href="javascript:void(0)" class="btn btn--m" @click="mdShowCart=false">继续购物</a>
+          <router-link href="javascript:void(0)" class="btn btn--m" to="/cart">查看购物车</router-link>
+        </div>
+      </modal>
       <nav-footer></nav-footer>
     </div>
 </template>
+<style>
+.load-more{
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+}
+.sort-up{
+  transform: rotate(180deg);
+  transition:all .3s ease-out
+}
+</style>
+
 <script>
 import NavHeader from '@/components/NavHeader.vue'
 import NavFooter from '@/components/NavFooter.vue'
 import NavBread from '@/components/NavBread.vue'
-
+import Modal from '@/components/Modal.vue'
+import axios from 'axios'
+import { setTimeout } from 'timers';
     export default{
         data(){
             return {
+              goodsList:[],
+              loading:false,
+              mdShow:false,
+              mdShowCart:false,
+              priceFilter:[
+                {
+                  startPrice:'0.00',
+                  endPrice:'100.00'
+                },
+                {
+                  startPrice:'100.00',
+                  endPrice:'500.00'
+                },
+                {
+                  startPrice:'500.00',
+                  endPrice:'1000.00'
+                },
+                {
+                  startPrice:'1000.00',
+                  endPrice:'2000.00'
+                },
+              ],
+              priceChecked:'all',
+              filterBy:false,
+              overLayFlag:false,
+              // 排序部分
+              sortFlag: true,
+              page: 1,
+              pageSize: 8,
+              busy:true,
 
             }
         },
         components:{
           NavHeader,
           NavFooter,
-          NavBread
+          NavBread,
+          Modal
+        },
+        mounted: function(){
+          this.getCoodList()
+        },
+        methods:{
+          getCoodList(flag){
+            this.loading = true
+            var param ={
+              page:this.page,
+              pageSize:this.pageSize,
+              sort:this.sortFlag?1:-1,
+              priceLevel:this.priceChecked
+            }
+            axios.get('/goods/list',{params:param}).then((result)=>{
+              this.loading = false
+              let res = result.data;
+              if(res.status=="0"){
+                if(flag){
+                  this.goodsList = this.goodsList.concat(res.result.list);
+                  if(res.result.count==0){
+                    console.log("enough")
+                    this.busy=true;
+                  }else{
+                    this.busy = false
+                  }
+                }else{
+                  this.goodsList = res.result.list
+                  this.busy = false
+                }
+
+              }else{
+                this.goodsList = []
+              }
+              
+            })
+          },
+          showFilterPop(){
+            this.filterBy = true;
+            this.overLayFlag = true;
+          },
+          closePop(){
+            this.filterBy = false;
+            this.overLayFlag = false;
+          },
+          setPriceFilter(index){
+            this.priceChecked = index;
+            this.closePop()
+            this.page = 1;
+            this.getCoodList();
+          },
+          sortGoods(){
+            this.sortFlag = !this.sortFlag;
+            this.page = 1;
+            this.getCoodList();
+
+          },
+          loadMore(){
+            this.busy = true;
+            setTimeout(() =>{
+              this.page++;
+              this.getCoodList(true)
+            },500);
+          },
+          addCart(productId){
+            axios.post("/goods/addCart",{
+              productId:productId
+            }).then((res)=>{
+              console.log(res);
+              if(res.data.status==0){
+                this.mdShowCart=true
+              }else{
+                this.mdShow=true
+              }
+            })
+          },
+          closeModal(){
+            this.mdShow=false;
+          }
         }
     }
 </script>
